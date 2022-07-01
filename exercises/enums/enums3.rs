a8580c664e72ba1a7f1e9f097d1efda605ecfaa0
Doug Morand <doug@commure.com>
2022-07-01 11:48:01 -0400

    Recent rustlings exercises

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,12 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -21,6 +23,7 @@ struct State {
 impl State {
     fn change_color(&mut self, color: (u8, u8, u8)) {
         self.color = color;
+        println!("Changed color to {:?}", color)
     }
 
     fn quit(&mut self) {
@@ -36,7 +39,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor((r, g, b)) => self.change_color((r, g, b)),
+            Message::Move(p) => self.move_position(p),
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit(),
+            _ => println!("Default behavior!"),
+        }
     }
 }
 
